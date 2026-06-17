561965402b84c924ab7f6057ef2aceda103f1fd8
Nikita <nikitasitchevoy@gmail.com>
2026-06-17 11:58:59 +0200

    test ci ok

---
+++ src/main/kotlin/your/package/infrastructure/driving/controller/GlobalExceptionHandler.kt
@@ -0,0 +1,21 @@
+package org.example.your.`package`.infrastructure.driving.controller
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleBadRequest(e: HttpMessageNotReadableException): Map<String, String?> =
+        mapOf("error" to e.message)
+
+    @ExceptionHandler(RuntimeException::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun handleRuntimeException(e: RuntimeException): Map<String, String?> =
+        mapOf("error" to e.message)
+}
